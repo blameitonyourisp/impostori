d758bbef44c12a9933d9f9abff5e5b57fb280ae8
James Reid <jamesreid3806@gmail.com>
2024-02-08 19:26:37 +0000

    int~ Refactor hint module

--- src/package/impostori/hint/cell.js
+++ src/package/impostori/hint/cell.js
@@ -1,27 +1,31 @@
-/** @license MIT */
-// Copyright (c) 2022 James Reid. All rights reserved.
+// Copyright (c) 2024 James Reid. All rights reserved.
 //
 // This source code file is licensed under the terms of the MIT license, a copy
-// of which may be found in the LICENSE.md file in the root of this repository.  
-// 
+// of which may be found in the LICENSE.md file in the root of this repository.
+//
 // For a template copy of the license see one of the following 3rd party sites:
-//      * <https://opensource.org/licenses/MIT>
-//      * <https://choosealicense.com/licenses/mit>
-//      * <https://spdx.org/licenses/MIT>
+//      - <https://opensource.org/licenses/MIT>
+//      - <https://choosealicense.com/licenses/mit>
+//      - <https://spdx.org/licenses/MIT>
+
+/**
+ * @file Fill cell hints.
+ * @author James Reid
+ */
 
 // @ts-check
 
-// @imports-types
-import { Grid, GridCell } from "../../types/index.js"
+// @@imports-package
+import { CELL_TYPES } from "../type/cell.js"
 
-// @body
+// @@imports-types
+/* eslint-disable no-unused-vars -- Types only used in comments. */
+import { Grid, GridCell } from "../../types/index.js"
+/* eslint-enable no-unused-vars -- Close disable-enable pair. */
+
+// @@body
 /**
- * 
- * @summary x
- * 
- * @function
- * @static
- * 
+ *
  * @param {Grid} grid
  * @param {number} index
  * @returns {GridCell}
@@ -31,7 +35,7 @@ const fillCellHints = (grid, index) => {
     const randomValueArray = grid.random.shuffledIndexArray(6, true)
     const hints = []
 
-    if (cell.type === "IMPOSTER") {
+    if (cell.type === CELL_TYPES.imposter) {
         const values = new Set()
         for (const index of cell.adjacentIndexes.all) {
             const cell = grid.cells[index]
@@ -40,33 +44,33 @@ const fillCellHints = (grid, index) => {
         const spies = grid.random
             .shuffleArray([...values.values()])
             .splice(0, 2)
-        for (let i = 0; i < 6; i ++) {
+        for (let i = 0; i < 6; i++) {
             const value = randomValueArray[i]
             if (value === cell.value || spies.includes(value)) { continue }
             hints.push(value)
         }
         hints.push(...spies)
-    } 
+    }
     else {
-        for (let i = 0; i < 6; i ++) {
+        for (let i = 0; i < 6; i++) {
             const value = randomValueArray[i]
             if (value === cell.value) { continue }
             hints.push(value)
         }
     }
 
-    const hintIndex = cell.type === "DETECTIVE" ? 0
-        : cell.type === "WORKER" ? grid.random.prng(1, 2)
+    const hintIndex = cell.type === CELL_TYPES.detective ? 0
+        : cell.type === CELL_TYPES.worker ? grid.random.prng(1, 2)
         : grid.random.prng(3, 5)
     hints.splice(hintIndex, 0, cell.value)
 
     const detective = hints.splice(0, 1)
     const worker = hints.splice(0, 2).sort()
     const imposter = hints.splice(0, 3).sort()
-    cell = { ...cell, hints: { detective, worker, imposter } } 
+    cell = { ...cell, hints: { detective, worker, imposter } }
 
     return cell
 }
 
-// @exports
+// @@exports
 export { fillCellHints }
--- src/package/impostori/hint/grid.js
+++ src/package/impostori/hint/grid.js
@@ -1,42 +1,46 @@
-/** @license MIT */
-// Copyright (c) 2022 James Reid. All rights reserved.
+// Copyright (c) 2024 James Reid. All rights reserved.
 //
 // This source code file is licensed under the terms of the MIT license, a copy
-// of which may be found in the LICENSE.md file in the root of this repository.  
-// 
+// of which may be found in the LICENSE.md file in the root of this repository.
+//
 // For a template copy of the license see one of the following 3rd party sites:
-//      * <https://opensource.org/licenses/MIT>
-//      * <https://choosealicense.com/licenses/mit>
-//      * <https://spdx.org/licenses/MIT>
+//      - <https://opensource.org/licenses/MIT>
+//      - <https://choosealicense.com/licenses/mit>
+//      - <https://spdx.org/licenses/MIT>
+
+/**
+ * @file Fill grid hints.
+ * @author James Reid
+ */
 
 // @ts-check
 
-// @imports-local
+// @@imports-package
 import { getAdjacencyData, requireGridAdjacency } from "../adjacency/index.js"
-// @imports-module
-import { fillCellHints } from "./cell.js"
-// @imports-types
-import { Grid } from "../../types/index.js"
+import { CELL_TYPES } from "../type/index.js"
 
-// @body
+// @@imports-module
+import { fillCellHints } from "./cell.js"
+
+// @@imports-types
+/* eslint-disable no-unused-vars -- Types only used in comments. */
+import { Grid } from "../../types/index.js"
+/* eslint-enable no-unused-vars -- Close disable-enable pair. */
+
+// @@body
 /**
- * 
- * @summary x
- * 
- * @function
- * @static
- * 
+ *
  * @param {Grid} grid
  * @returns {Grid}
  */
-const fillGridHints = (grid) => {
+const fillGridHints = grid => {
     grid = { ...grid, cells: [...grid.cells] }
-    for (let i = 0; i < 36; i ++) {
+    for (let i = 0; i < 36; i++) {
         const cell = fillCellHints(grid, i)
         grid.cells[i] = cell
-        if (cell.type === "IMPOSTER") {
+        if (cell.type === CELL_TYPES.imposter) {
             const spies = new Set(cell.hints.imposter.filter(value => {
-                return value != cell.value
+                return value !== cell.value
             }))
             for (const index of cell.adjacentIndexes.required) {
                 const adjacentCell = grid.cells[index]
@@ -55,8 +59,5 @@ const fillGridHints = (grid) => {
     return grid
 }
 
-// @exports
+// @@exports
 export { fillGridHints }
-
-
-
