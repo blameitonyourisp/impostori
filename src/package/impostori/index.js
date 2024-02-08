ef8f19f587f80f3b7cc8951bdc67a687b992232e
James Reid <jamesreid3806@gmail.com>
2024-02-08 19:31:08 +0000

    int~ Refactor impostori module

--- src/package/impostori/index.js
+++ src/package/impostori/index.js
@@ -1,3 +1,31 @@
+// Copyright (c) 2024 James Reid. All rights reserved.
+//
+// This source code file is licensed under the terms of the MIT license, a copy
+// of which may be found in the LICENSE.md file in the root of this repository.
+//
+// For a template copy of the license see one of the following 3rd party sites:
+//      - <https://opensource.org/licenses/MIT>
+//      - <https://choosealicense.com/licenses/mit>
+//      - <https://spdx.org/licenses/MIT>
+
 /**
- * @module impostori
+ * @file Export impostori module.
+ * @author James Reid
  */
+
+// @ts-check
+
+// @@no-imports
+
+// @@no-body
+
+// @@exports
+export * from "./adjacency/index.js"
+export * from "./generate/index.js"
+export * from "./grid/index.js"
+export * from "./hint/index.js"
+export * from "./reset/index.js"
+export * from "./serialization/index.js"
+export * from "./solve/index.js"
+export * from "./type/index.js"
+export * from "./value/index.js"
